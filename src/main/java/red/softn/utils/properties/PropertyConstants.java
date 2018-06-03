bf636210cde54cfd3f58f5f9193e6e2decaaaf69
Nicolás Marulanda P <nicomaru@outlook.com>
2018-06-03 21:28:15 +0200

    Refactoring

--- src/main/java/red/softn/utils/properties/PropertyConstants.java
+++ src/main/java/red/softn/utils/properties/PropertyConstants.java
@@ -1,14 +1,14 @@
-package red.softn.utils.files;
+package red.softn.utils.properties;
 
-public class PropertyKeysConstants {
+public class PropertyConstants {
     
     public static final String KEY_DIRECTORY = "project.directory";
     
     public static final String KEY_DIRECTORY_PACKAGES = "project.directory.packages";
     
-    public static final String KEY_PACKAGE = "project.package";
+    public static final String KEY_MAIN_PACKAGES = "project.main.packages";
     
-    public static final String KEY_PROJECT_CLASS_TYPE = "project.class.type";
+    public static final String KEY_CLASS_TYPE = "project.class.type";
     
     public static final String KEY_MODULE_FORMAT_POSITION_DIRECTORY = "project.module.format.position.directory";
     
@@ -18,14 +18,14 @@ public class PropertyKeysConstants {
     
     public static final String KEY_PROJECT_PACKAGE_SEPARATOR = "project.package.separator";
     
-    public static final String DEFAULT_VALUE_POSITION_DIRECTORY = "0";
+    public static final String DEFAULT_VALUE_MODULE_POSITION_DIRECTORY = "0";
     
-    public static final String DEFAULT_VALUE_POSITION_PACKAGE = "1";
+    public static final String DEFAULT_VALUE_MODULE_POSITION_PACKAGE = "1";
     
-    public static final String DEFAULT_VALUE_FORMAT_SEPARATOR = ":";
+    public static final String DEFAULT_VALUE_MODULE_FORMAT_SEPARATOR = ":";
     
-    public static final String DEFAULT_VALUE_PACKAGE_SEPARATOR = ".";
+    public static final String VALUE_DOT_SEPARATOR = ".";
     
-    public static final String REGEX_PACKAGE_SEPARATOR = "\\.";
+    public static final String REGEX_DOT_SEPARATOR = "\\.";
     
 }
